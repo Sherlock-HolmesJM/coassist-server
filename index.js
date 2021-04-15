1222236146e96075c8761318bf47699b05f9ad63
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-04-15 14:29:13 +0100

    port fixed

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get('/api/excel', async (req, res) => {
   }
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
