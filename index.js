1a53efa0ea61725dc202fd067064c82753f4bed2
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-19 04:00:25 +0100

    added new endpoint for metadata

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const { getWorkbook } = require('./report');
+const express = require("express");
+const cors = require("cors");
+const { getWorkbook } = require("./report");
+
+const youtube = require("youtube-metadata-from-url");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.get('/api/excel', async (req, res) => {
+app.get("/api/excel", async (req, res) => {
   try {
     const buffer = await getWorkbook();
     res.json(buffer);
@@ -19,6 +21,19 @@ app.get('/api/excel', async (req, res) => {
     console.log(e);
     res.send(e);
   }
+});
+
+app.get("/api/metadata/:id", async (req, res) => {
+  const url = `https://www.youtube.com/watch?v=${req.params.id}`;
+
+  youtube.metadata(url).then(
+    function (json) {
+      res.send(json);
+    },
+    function (err) {
+      res.send(err);
+    }
+  );
 });
 
 const port = process.env.PORT || 5000;
